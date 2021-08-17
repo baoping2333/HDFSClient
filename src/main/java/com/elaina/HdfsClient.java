74a944a91dc5be5f07dd895929025d0a75e68ff0
pi <1481273127@qq.com>
2021-08-18 00:59:06 +0800

    随便上传

--- src/main/java/com/elaina/HdfsClient.java
+++ src/main/java/com/elaina/HdfsClient.java
@@ -3,6 +3,8 @@ package com.elaina;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -18,21 +20,27 @@ import java.net.URISyntaxException;
  * @Version 1.0
  **/
 public class HdfsClient {
+    private FileSystem fs;
+
+    @Before
+    public void init() throws IOException, InterruptedException, URISyntaxException {
+        Configuration configuration = new Configuration();
+        fs = FileSystem.get(new URI("hdfs://hadoop102:9820"), configuration, "atguigu");
+    }
+
+    @After
+    public void close() throws Exception {
+        fs.close();
+    }
+
     @Test
     public void testMkdirs() throws IOException, InterruptedException, URISyntaxException {
-        // 1 获取文件系统
-        Configuration configuration = new Configuration();
-        // 配置在集群上运行
-        // configuration.set("fs.defaultFS", "hdfs://hadoop102:9820");
-        // FileSystem fs = FileSystem.get(configuration);
 
-        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:9820"), configuration, "atguigu");
-//1213
         // 2 创建目录
-        fs.mkdirs(new Path("/1108/daxian/banzhang"));
+        fs.mkdirs(new Path("/2021/linzhi"));
 
         // 3 关闭资源
-        fs.close();
+
     }
 
 }
